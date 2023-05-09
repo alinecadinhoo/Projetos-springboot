4132d2af5ce97a3e11b2ded4d4aa26d0c8a01c7f
Aline Cadinho <cadinhoaline@gmail.com>
2023-05-08 23:44:22 -0300

    springboot

---
+++ meu-projeto-spring1/src/main/java/br/com/meuPrimeiroProjeto/meuprojetospring1/MeuProjetoSpring1Application.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojetospring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpring1Application.class, args);
+	}
+
+}
---
+++ meu-projeto-spring1/src/test/java/br/com/meuPrimeiroProjeto/meuprojetospring1/MeuProjetoSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojetospring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
